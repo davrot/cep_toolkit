d00841cb089eb215b58cd7edc8cc18a412c58afe
David Rotermund <davrot@uni-bremen.de>
2026-07-25 05:59:33 +0200

    More scripts

---
+++ cron_check_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# List of expected container names
+expected_containers=( "nginx" "sharelatex" "mongo" "redis" "git-bridge" "languagetool")
+
+# Email settings
+recipient="overleaf@uni-bremen.de"
+subject="Docker Container Alert"
+
+# Check containers
+for container in "${expected_containers[@]}"; do
+    if ! docker ps --format '{{.Names}}' | grep -q "^$container$"; then
+        echo "Container $container is not running" | mail -s "$subject" "$recipient"
+    fi
+done
+
